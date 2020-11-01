2713aafc84380486f72eae9f816b4f404846cbf5
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-11-01 22:01:39 +0300

    add addTodo project feature

--- src/design/project.js
+++ src/design/project.js
@@ -1,18 +1,66 @@
 
+let todos = [];
+const todoUI = () => {
 
-const project = () =>{
-    const addProject = document.querySelector('.projectForm');
-    const projectForm = document.createElement('form');
-    const projectName = document.createElement('input');
-    const addPBtn = document.createElement('button');
-    addPBtn.setAttribute('type','button');
-    addPBtn.innerText = "Add a project"
-    projectName.setAttribute('placeholder','Enter a project name')
-    projectForm.appendChild(projectName);
-    projectForm.appendChild(addPBtn);
-    addProject.appendChild(projectForm);
-}
+    const saveTodoProject = (todo) => {
+        let project = localStorage.getItem('project');
+        if (!project) {
+            project = localStorage.setItem('project', '{"todos": null}');
+            project = localStorage.getItem('project');
+        }
 
-document.getElementById('add-project').addEventListener('click',project)
+        project = JSON.parse(project);
 
-export default project
+        project = {...project.todos, todo};
+        localStorage.setItem('project', JSON.stringify(project));
+    };
+
+    const addProject = () => {
+        const projectName = document.getElementById('projectName');
+        if (projectName.value == '') {
+            return;
+        }
+
+        saveTodoProject({projectName: projectName.value})
+        projectName.value = '';
+    };
+
+    const createAddProjectForm = () => {
+        const addProjectExist = document.querySelector('.project-form');
+
+        if (addProjectExist) {
+            return;
+        }
+
+        const modal = document.querySelector('.modal');
+
+        const projectFormWrapper = document.createElement('div');
+        projectFormWrapper.classList.add('project-form');
+
+        const projectForm = document.createElement('form');
+        const projectName = document.createElement('input');
+        const addPBtn = document.createElement('button');
+        
+        addPBtn.setAttribute('type','button');
+        addPBtn.addEventListener('click', addProject);
+        addPBtn.innerText = "Add a project";
+
+        projectName.setAttribute('placeholder','Enter a project name');
+        projectName.setAttribute('id', 'projectName');
+
+        projectForm.appendChild(projectName);
+        projectForm.appendChild(addPBtn);
+        projectFormWrapper.appendChild(projectForm);
+        modal.appendChild(projectFormWrapper);
+    };
+
+    return {createAddProjectForm}
+   
+};
+
+
+const todoForm = todoUI();
+
+document.getElementById('add-project').addEventListener('click', todoForm.createAddProjectForm)
+
+export default todoUI
--- src/main.js
+++ src/main.js
@@ -1,3 +1,1 @@
 import project from './design/project';
-
-project();
