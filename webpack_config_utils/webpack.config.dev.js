df255d9b23eb39352b9aa91f5e7f8729dce6f30c
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-11-01 16:10:54 +0300

    add setup

---
+++ webpack_config_utils/webpack.config.dev.js
@@ -0,0 +1,34 @@
+const appPath = require('./common.path');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
+const glob = require('glob');
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    contentBase: appPath.OUTPUT,
+    compress: true,
+    port: 3001,
+    // hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+          'postcss-loader'
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlPlugin({
+      title: 'Todo Application',
+      template: `${appPath.ENTRY_SRC}/template.html`
+    }),
+  ]
+}
