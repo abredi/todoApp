ee259035d00b24243aec9096f407cf998b636ce2
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-11-06 13:25:37 +0300

    refactor helper.js

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -21,13 +21,11 @@ const createListItem = (project) => {
 
 const cancelModal = (event) => {
   event.preventDefault();
-
   cleanModal();
-
-  console.log('cancel modal clicked');
 };
 
-const TailwindButtonClass = `inline-flex justify-center w-full px-4 py-2 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-purple-600 border border-transparent rounded-md shadow-sm hover:bg-purple-500 focus:outline-none focus:border-purple-700 focus:shadow-outline-red sm:text-sm sm:leading-5`.split(' ');
+const TailwindButtonClass = `inline-flex justify-center w-full px-4 py-2 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-purple-600 border border-transparent rounded-md shadow-sm hover:bg-purple-500 focus:outline-none focus:border-purple-700 focus:shadow-outline-red sm:text-sm sm:leading-5`.split(
+  " "
+);
 
 export { cancelModal, cleanModal, createListItem, TailwindButtonClass };
-
