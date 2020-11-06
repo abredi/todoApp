5d4891c71e02de1ffbd81b2d932f45ec99f03b56
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-11-07 01:24:54 +0300

    update delete feature, update project form ui, update task form ui

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -1,3 +1,5 @@
+import { local } from "../storage/local";
+
 const cleanModal = (id = "modalStation") => {
   const modal = document.getElementById(id);
   while (modal.childElementCount > 0) {
@@ -24,7 +26,7 @@ const cancelModal = (event) => {
   cleanModal();
 };
 
-const createSelectElement = (ary, key, value, selectedValue = null) => {
+const createSelectElement = (ary, key, value, selectedValue = null, lable = null) => {
   const selectElem = document.createElement("select");
   selectElem.setAttribute("required", "required");
   const optDisabled = document.createElement("option");
@@ -32,10 +34,10 @@ const createSelectElement = (ary, key, value, selectedValue = null) => {
   if (!selectedValue) {
     optDisabled.selected = "true";
   }
-  
-  optDisabled.innerText = "Choose One";
+
+  optDisabled.innerText = `Choose ${lable}`;
   selectElem.appendChild(optDisabled);
-  selectElem.classList.add("border", "border-indigo-500");
+  selectElem.classList.add("border-b", "border-indigo-500");
 
   if (ary) {
     ary.map((item) => {
@@ -51,14 +53,73 @@ const createSelectElement = (ary, key, value, selectedValue = null) => {
   return selectElem;
 };
 
+const createUL = (project) => {
+  const ul = document.createElement("ul");
+  ul.classList.add(
+    "flex-row",
+    "divide-y-2",
+    "divide-indigo-200",
+    "divide-dashed"
+  );
+  ul.setAttribute("id", project.projectId);
+
+  return ul;
+};
+
+const deleteProject = (event) => {
+  event.preventDefault();
+
+  const ls = local();
+
+  const pid = event.target.getAttribute("data-pid");
+
+  if (pid) {
+    ls.deleteProjectById(pid);
+    const projectCard = document.querySelector(`[data-card-pid="${pid}"]`);
+    const projectList = document.querySelector(`a[data="${pid}"]`);
+    if (projectCard) {
+      projectCard.parentElement.removeChild(projectCard);
+      projectList.parentElement.parentElement.removeChild(projectList.parentElement);
+    }
+  }
+  return false;
+};
+
+const createCardHeader = (project) => {
+  const cardHeader = document.createElement("div");
+  cardHeader.classList.add(
+    "flex",
+    "items-center",
+    "justify-between",
+    "px-8",
+    "py-4",
+    "bg-gray-200",
+    "cursor-pointer"
+  );
+  const delElem = document.createElement("button");
+  delElem.setAttribute("data-pid", project.projectId);
+  delElem.classList.add("text-sm", "text-red-600", "focus:outline-none");
+  delElem.innerText = "Delete";
+  delElem.addEventListener("click", deleteProject);
+  const projectNameElem = document.createElement("h2");
+  projectNameElem.classList.add("text-2xl", "font-light", "text-gray-700");
+  projectNameElem.innerText = project.projectName;
+  cardHeader.appendChild(projectNameElem);
+  cardHeader.appendChild(delElem);
+  return cardHeader;
+};
+
 const TailwindButtonClass = `inline-flex justify-center w-full px-4 py-2 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-red sm:text-sm sm:leading-5`.split(
   " "
 );
 
 export {
+  deleteProject,
   createSelectElement,
   cancelModal,
   cleanModal,
   createListItem,
   TailwindButtonClass,
+  createCardHeader,
+  createUL,
 };
