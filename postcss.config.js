fd05991779a685744598b0d90e852e4f37c912b2
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-11-07 02:45:18 +0300

    add postcss and purgecss configuration

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,15 @@ const tailwindcss = require('tailwindcss');
 
 module.exports = {
   plugins: [
-    tailwindcss('./tailwind.config.js'),
-    require('autoprefixer'),
-    
-  ]
-}
+    tailwindcss("./tailwind.config.js"),
+    require("autoprefixer"),
+    require("@fullhuman/postcss-purgecss")({
+      content: [
+        "./src/*.html",
+        "./src/design/**/*.js",
+        "./src/util/**/*.js",
+      ],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
+  ],
+};
